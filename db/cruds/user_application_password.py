6cb8374161cec77bc579bb52b0fee04914dceb79
RTa-technology <scpfoundationidcardmaker@gmail.com>
2023-02-10 21:12:17 +0900

    feat: add application_hash

--- db/cruds/user_application_password.py
+++ db/cruds/user_application_password.py
@@ -4,9 +4,15 @@ import random
 from db.engine import Session
 from db.models import UserApplicationPassword
 
+import mmh3
+
 CHAR_BORDER = "\u200B"
 CHAR_ZERO = "\u200C"
 CHAR_ONE = "\u200D"
+
+HASH_BORDER = "\u2060"
+HASH_ZERO = "\u2061"
+HASH_ONE = "\u2062"
 
 
 def _get_passwords() -> dict:
@@ -23,12 +29,21 @@ def _get_passwords() -> dict:
 
 def encode(password: str, user_id: int) -> str:
     encoded_user_id = str(bin(user_id)[2:]).replace("0", CHAR_ZERO).replace("1", CHAR_ONE)
+    hashed_password = mmh3.hash(password+str(user_id), os.getenv("APPLICATION_HASH_SEED"), signed=False)
+    encoded_hashed_password = str(bin(hashed_password)[2:]).replace("0", HASH_ZERO).replace("1", HASH_ONE)
     index = random.randint(1, len(password) - 2)
-    return password[:index] + CHAR_BORDER + encoded_user_id + CHAR_BORDER + password[index:]
+    return password[:index] + CHAR_BORDER + encoded_user_id + CHAR_BORDER + HASH_BORDER + encoded_hashed_password + HASH_BORDER + password[index:]
 
 
 def decode(password: str) -> int:
     return int(password.split(CHAR_BORDER)[1].replace(CHAR_ZERO, "0").replace(CHAR_ONE, "1"), 2)
+
+# def decode_hash(password: str) -> bool:
+#     user_id = int(password.split(CHAR_BORDER)[1].replace(CHAR_ZERO, "0").replace(CHAR_ONE, "1"), 2)
+#     raw_password = password.split(CHAR_BORDER)[0] + password.split(HASH_BORDER)[2]
+#     raw_hash = int(password.split(HASH_BORDER)[1].replace(HASH_ZERO, "0").replace(HASH_ONE, "1"), 2)
+#     hashed_password = mmh3.hash(raw_password+str(user_id), os.getenv("APPLICATION_HASH_SEED"), signed=False)
+#     return raw_hash == hashed_password
 
 
 def get(user_id: int):
